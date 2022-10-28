6f6b4c55f7c9d5a154cdae4f3aef61618b9243a8
unknown <kppena14@gmail.com>
2022-10-27 23:10:54 -0400

    Complete Discount Applier class and test class

--- src/main/java/tudelft/discount/DiscountApplier.java
+++ src/main/java/tudelft/discount/DiscountApplier.java
@@ -1,6 +1,10 @@
 package tudelft.discount;
 
-public class DiscountApplier {
+import java.util.ArrayList;
+import java.util.List;
+
+public class DiscountApplier
+{
 
     private ProductDao dao;
 
@@ -8,16 +12,26 @@ public class DiscountApplier {
         this.dao = dao;
     }
 
-    public void setNewPrices() {
+    public List<Product> setNewPrices()
+    {
 
-        for(Product product : dao.all()) {
-            if(product.getCategory().equals("BUSINESS")) {
-                product.setPrice(product.getPrice() * 0.9);
-            }
-            if(product.getCategory().equals("HOME")) {
+        List<Product> NewPrice = new ArrayList<>();
+
+
+        for(Product product : dao.all())
+        {
+            if(product.getCategory().equals("BUSINESS"))
+            {
                 product.setPrice(product.getPrice() * 1.1);
             }
+            if(product.getCategory().equals("HOME"))
+            {
+                product.setPrice(product.getPrice() * 0.9);
+            }
+            NewPrice.add(product);
+
         }
+        return NewPrice;
 
     }
 }
--- src/test/java/tudelft/discount/DiscountApplierTest.java
+++ src/test/java/tudelft/discount/DiscountApplierTest.java
@@ -1,5 +1,31 @@
 package tudelft.discount;
 
-public class DiscountApplierTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class DiscountApplierTest
+{
+    @Test
+    void ApplierTest()
+    {
+        Product suit = new Product("Suit", 100, "BUSINESS");
+        Product blender = new Product("Blender", 50, "HOME");
+
+        ProductDao dao = Mockito.mock(ProductDao.class);
+
+        List<Product> results = Arrays.asList(suit, blender);
+        Mockito.when(dao.all()).thenReturn(results);
+
+        DiscountApplier applier = new DiscountApplier(dao);
+        List<Product> result = applier.setNewPrices();
+
+        Assertions.assertEquals(110.00000000000001, suit.getPrice());
+        Assertions.assertEquals(45, blender.getPrice());
+
+    }
 
 }

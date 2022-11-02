923f9ffa2419c273a22ac1182e0e5d395b267389
unknown <kppena14@gmail.com>
2022-11-01 22:34:36 -0400

    Fixed Mirror Class and Mirror Test Class

--- src/main/java/tudelft/mirror/Mirror.java
+++ src/main/java/tudelft/mirror/Mirror.java
@@ -1,21 +1,26 @@
 package tudelft.mirror;
 
-public class Mirror {
+public class Mirror
+{
 
-    public String mirrorEnds(String string) {
+    public String mirrorEnds(String string)
+    {
         String mirror = "";
 
         int begin = 0;
         int end = string.length() - 1;
-        for (; begin < end; begin++, end--) {
-            if (string.charAt(begin) == string.charAt(end)) {
+        for (int i = 0; i < string.length(); i++, begin++, end--)
+        {
+            if (string.charAt(begin) == string.charAt(end))
+            {
                 mirror += String.valueOf(string.charAt(end));
             }
-            else {
+            else
+            {
                 break;
             }
         }
 
-        return begin == end ? string : mirror;
+        return mirror;
     }
 }
--- src/test/java/tudelft/mirror/MirrorTest.java
+++ src/test/java/tudelft/mirror/MirrorTest.java
@@ -1,4 +1,26 @@
 package tudelft.mirror;
 
-public class MirrorTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import tudelft.chocolate.ChocolateBags;
+
+public class MirrorTest
+{
+    @ParameterizedTest(name = "({0}) = ({1})")
+    @CsvSource({
+            // The total is higher than the amount of small and big bars.
+            "'AAAA', 'AAAA'", "'abWXYZba', 'ab'", "'abaWXYZaba', 'aba'",
+            // No need for small bars.
+            "'abca','a'", "'abcda','a'", "'abcdea','a'",
+            // Need for big and small bars.
+            "'aba','aba'", "'abba','abba'", "'abcba','abcba'"
+    })
+    public void testmirror(String string, String expectedResult)
+    {
+        String result = new Mirror().mirrorEnds(string);
+        Assertions.assertEquals(expectedResult, result);
+    }
+
 }
